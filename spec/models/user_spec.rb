2a33044ddbb1b14b72778c09045fe016d51c0f7c
fukaya.masato <coolbanana4092@gmail.com>
2019-02-16 04:37:57 +0900

    RSpec:Userモデルの新規登録機能に関するテスト

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # 名前、メールアドレス、パスワードがあれば有効であること
+  it "is valid with a name, email, and password" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
+  # 名前がなければ無効であること
+  it "is invalid without a name" do
+    user = FactoryBot.build(:user, name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  # メールアドレスがなければ無効であること
+  it "is invalid without an email address" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  # メールアドレスの形式でなければ無効であること
+  it "is invalid without mail address format" do
+    user = FactoryBot.build(:user, email: "m-fukaya-com")
+    user.valid?
+    expect(user.errors[:email]).to include("is invalid")
+  end
+
+  # 重複したメールアドレスなら無効であること
+  it "is invalid with a duplicate email address" do
+    FactoryBot.create(:user, email: "m-fukaya@gmail.com")
+    user = FactoryBot.build(:user, email: "m-fukaya@gmail.com")
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  # パスワードがなければ無効であること
+  it "is invalid without an password_digest" do
+    user = FactoryBot.build(:user, password_digest: nil)
+    user.valid?
+    expect(user.errors[:password_digest]).to include("can't be blank")
+  end
+end
