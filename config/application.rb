2b80bf4a55ed2c29d5ca9348349d2bcd27bd7e36
fukaya.masato <coolbanana409@outlook.jp>
2019-02-16 01:34:39 +0900

    [修正版]RSpecのセットアップ

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,11 @@ module Stock
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    view_specs: false,
-    helper_specs: false,
-    routing_specs: false
+    config.generators do |g|
+      g.test_framework :rspec,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
