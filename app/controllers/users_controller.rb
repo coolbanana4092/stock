d4bde51f92a0dc13dc8501c67cd1baa5b1d5aaff
fukaya.masato <coolbanana409@outlook.jp>
2019-02-17 23:45:02 +0900

    新規登録時に永続的セッションを使用した上でログイン状態にする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  include SessionsHelper
 
   def new
     @user = User.new
@@ -8,6 +9,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
+      remember(@user)
       redirect_to root_url
     else
       redirect_to signup_url
