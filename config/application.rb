d2b705f01139f46773c328014dc56a2c42febf1d
fukaya.masato <coolbanana4092@gmail.com>
2019-02-26 21:34:42 +0900

    RSpec:コントローラースペックの削除

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module Stock
       g.test_framework :rspec,
       view_specs: false,
       helper_specs: false,
-      routing_specs: false
+      routing_specs: false,
+      controller_specs: false
     end
   end
 end
