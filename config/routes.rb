411f397a63281d6aba2a16a57337d3cbcbb7466f
fukaya.masato <coolbanana4092@gmail.com>
2019-03-10 22:42:00 +0900

    ユーザーの情報を編集できる様にする

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:new, :create]
+
+  resources :users, only: [:new, :create, :edit, :update]
   resources :password_resets, only: [:new, :create, :edit, :update]
 end
