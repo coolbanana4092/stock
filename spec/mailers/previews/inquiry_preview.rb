55825bade7717e3b35e9194f0fd9cce03d4db56b
fukaya.masato <coolbanana4092@gmail.com>
2019-03-14 03:51:18 +0900

    RSpec:お問い合わせ機能を実装する

--- spec/mailers/previews/inquiry_preview.rb
+++ spec/mailers/previews/inquiry_preview.rb
@@ -1,7 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/inquiry
 class InquiryPreview < ActionMailer::Preview
+  # Preview all emails at http://localhost:3000/rails/mailers/inquiry/inquiry
   def inquiry
-    inquiry = Inquiry.new(name: "m-fukaya", message: "バグが発生しています。")
+    inquiry = Inquiry.new(name: "m-fukaya", email: "m-fukaya@gmail.com", message: "バグが発生しています。")
 
     InquiryMailer.send_email(inquiry)
   end
