75d4e8c259ae437b5bd947d7361d506a1cb83f24
fukaya.masato <coolbanana4092@gmail.com>
2019-08-29 14:48:40 +0900

    イベントの日時検索に関わっているコントローラーとモデル内の記述を簡略化する

--- app/controllers/finds_controller.rb
+++ app/controllers/finds_controller.rb
@@ -2,21 +2,8 @@ class FindsController < ApplicationController
   before_action :require_user_logged_in, only: [:index]
 
   def index
-    # いろいろな方法を試したが、以下の上から6つの似た様なインスタンス変数を
-    # judge_created_atの様なメソッドに置き換えて、1つのインスタンス変数に適切sにまとめることができなかったので、
-    # とりあえずそのまま6つとも記しておく。
-    if params[:created_at] == "today"
-      @events = Event.all.order(created_at: :desc).page(params[:page]).per(10).judge_created_at_today
-    elsif params[:created_at] == "3_days_later"
-      @events = Event.all.order(created_at: :desc).page(params[:page]).per(10).judge_created_at_3d
-    elsif params[:created_at] == "1_week_later"
-      @events = Event.all.order(created_at: :desc).page(params[:page]).per(10).judge_created_at_1w
-    elsif params[:created_at] == "1_month_later"
-      @events = Event.all.order(created_at: :desc).page(params[:page]).per(10).judge_created_at_1m
-    elsif params[:created_at] == "3_months_later"
-      @events = Event.all.order(created_at: :desc).page(params[:page]).per(10).judge_created_at_3m
-    elsif params[:created_at] == "6_months_later"
-      @events = Event.all.order(created_at: :desc).page(params[:page]).per(10).judge_created_at_6m
+    if params[:created_at]
+      @events = Event.all.order(created_at: :desc).page(params[:page]).per(10).judge_created_at(params[:created_at])
     else
       @events = Event.all.order(created_at: :desc).page(params[:page]).per(10)
     end
